1dc6e0e51d6840d696735a44fdd45fbf365ee5fb
Dennis Vriend <dnvriend@gmail.com>
2025-11-15 19:39:20 +0100

    Initial commit from cookiecutter-python-cli-uv

---
+++ aws_primitives_tool/__init__.py
@@ -0,0 +1,7 @@
+"""aws-primitives-tool: A CLI that provides AWS serverless primitives as composable CLI commands
+
+Note: This code was generated with assistance from AI coding tools
+and has been reviewed and tested by a human.
+"""
+
+__version__ = "0.1.0"
